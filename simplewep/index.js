758fa0b46bbf817e9af81f0a75f49a865cb50e6a
Abdulvorisjs <abdulvoris000urolov@gmail.com>
2021-10-02 19:01:07 +0500

    add

--- simplewep/index.js
+++ simplewep/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hi There');
+    res.send('How are you doing ');
 });
 
 app.listen(8080, ()=>{
